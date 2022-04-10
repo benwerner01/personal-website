1bea3fe18b43a6da066294f44bb497e699ceb809
Ben Werner <ben.werner01@gmail.com>
2022-04-10 16:40:23 +0100

    f1 next/prev page buttons

--- src/pages/f1/[year].page.tsx
+++ src/pages/f1/[year].page.tsx
@@ -1,5 +1,13 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import { Container, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  styled,
+  Container,
+  Typography,
+  ButtonProps,
+} from "@mui/material";
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
 import {
   fetchSeasonRaceResults,
   fetchSeasonRaceResultsByDriver,
@@ -10,9 +18,20 @@ import {
 } from "./shared/ergastF1Api";
 import DriverRaceResultsLineGraph from "./shared/DriverRaceResultsLineGraph";
 import { f1Color } from "./shared/util";
+import Link from "next/link";
+
+const F1RedButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(f1Color),
+  backgroundColor: f1Color,
+  boxShadow: "none",
+  "&:hover": {
+    backgroundColor: f1Color,
+    boxShadow: "none",
+  },
+}));
 
 type F1PageProps = {
-  year;
+  year: string;
   seasonRaceResults: SeasonRaceResults;
   seasonRaceResultsByDriver: RaceDriverWithResultsAndConstructor[];
   seasonRaces: SeasonRaces;
@@ -49,11 +68,61 @@ const F1Page: NextPage<F1PageProps> = ({
   seasonRaceResultsByDriver,
   seasonRaces,
 }) => {
+  const yearAsNumber = parseInt(year, 10);
+
   return (
     <Container sx={{ position: "relative" }}>
-      <Typography variant="h1" sx={{ color: f1Color }}>
-        {seasonRaceResults.season} F1 Season
-      </Typography>
+      <Box display="flex">
+        <Link href={`/f1/${yearAsNumber - 1}`}>
+          <a>
+            <F1RedButton
+              variant="contained"
+              sx={{
+                px: 1,
+                minWidth: "unset",
+                borderTopRightRadius: 0,
+                borderBottomRightRadius: 0,
+              }}
+              color="inherit"
+            >
+              <ChevronLeft />
+            </F1RedButton>
+          </a>
+        </Link>
+        <Box
+          sx={{
+            width: 75,
+            borderColor: f1Color,
+            borderStyle: "solid",
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography sx={{ color: f1Color }}>
+            <strong>{seasonRaceResults.season}</strong>
+          </Typography>
+        </Box>
+        <Link href={`/f1/${yearAsNumber + 1}`}>
+          <a>
+            <F1RedButton
+              variant="contained"
+              disabled={new Date().getFullYear() === yearAsNumber}
+              sx={{
+                px: 1,
+                minWidth: "unset",
+                borderTopLeftRadius: 0,
+                borderBottomLeftRadius: 0,
+              }}
+              color="inherit"
+            >
+              <ChevronRight />
+            </F1RedButton>
+          </a>
+        </Link>
+      </Box>
       <DriverRaceResultsLineGraph
         year={year}
         seasonRaceResultsByDriver={seasonRaceResultsByDriver}
